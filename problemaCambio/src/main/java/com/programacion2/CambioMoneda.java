12de10a720143ced2dcd028f4e53312810086402
jjoana <jesus.joanaazuara@usp.ceu.es>
2023-06-20 11:36:49 +0200

    Ejercicio de cambio de moneda terminado

---
+++ problemaCambio/src/main/java/com/programacion2/CambioMoneda.java
@@ -0,0 +1,56 @@
+package com.programacion2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class CambioMoneda {
+
+    public static int cambioIterativo(int cambio, List<Integer> sistemaMonedas) {
+        int resultado = 0;
+        for (int i = 0; i < sistemaMonedas.size(); i++) {
+            while (cambio > 0) {
+                cambio -= sistemaMonedas.get(i);
+                resultado++;
+            }
+        }
+        return resultado;
+    }
+
+    public static int cambioRecursivo(int cambio, List<Integer> sistemaMonedas) {
+        int[] resultado = new int[1];
+        cambioRecursivo(cambio, 0, sistemaMonedas, resultado);
+        return resultado[0];
+    }
+
+    private static void cambioRecursivo(int cambio, int i, List<Integer> sistemaMonedas, int resultado[]) {
+        if (cambio > 0) {
+            int elemento = sistemaMonedas.get(i);
+            if (cambio >= elemento) {
+                resultado[0] += cambio / sistemaMonedas.get(i);
+                cambio = cambio % sistemaMonedas.get(i);
+                cambioRecursivo(cambio, ++i, sistemaMonedas, resultado);
+            } else {
+                cambioRecursivo(cambio, ++i, sistemaMonedas, resultado);
+            }
+        }
+    }
+
+    public static int cambioLambda(int cambio, List<Integer> sistemaMonedas) {
+        BiFunction<Integer, List<Integer>, Integer> interF = (a, b) -> {
+            int resultado = 0;
+            for (int i = 0; i < b.size(); i++) {
+                while (a > 0) {
+                    a -= b.get(i);
+                    resultado++;
+                }
+            }
+            return resultado;
+
+        };
+         return interF.apply(cambio, sistemaMonedas);
+
+    }
+}
---
+++ problemaCambio/src/test/java/com/programacion2/CambioMonedaTest.java
@@ -0,0 +1,147 @@
+package com.programacion2;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+public class CambioMonedaTest {
+
+    @Test
+    void cambioMonedaTest1() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(2, CambioMoneda.cambioIterativo(5, miLista));
+    }
+
+    @Test
+    void cambioMonedaTest2() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(0, CambioMoneda.cambioIterativo(0, miLista));
+    }
+
+    @Test
+    void cambioMonedaTest3() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(1, CambioMoneda.cambioIterativo(1, miLista));
+    }
+
+    @Test
+    void cambioMonedaTest4() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(6);
+        miLista.add(4);
+        miLista.add(2);
+        miLista.add(1);
+
+        
+        assertEquals(3, CambioMoneda.cambioIterativo(13, miLista));
+    }
+
+    @Test
+    void cambioMonedaRTest1() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(2, CambioMoneda.cambioRecursivo(5, miLista));
+    }
+
+    @Test
+    void cambioMonedaRTest2() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(0, CambioMoneda.cambioRecursivo(0, miLista));
+    }
+
+    @Test
+    void cambioMonedaRTest3() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(1, CambioMoneda.cambioRecursivo(1, miLista));
+    }
+
+    @Test
+    void cambioMonedaRTest4() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(6);
+        miLista.add(4);
+        miLista.add(2);
+        miLista.add(1);
+
+        assertEquals(3, CambioMoneda.cambioRecursivo(13, miLista));
+    }
+
+
+    @Test
+    void cambioLambdaTest1() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(2, CambioMoneda.cambioLambda(5, miLista));
+    }
+
+    @Test
+    void cambioLambdaTest2() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(0, CambioMoneda.cambioLambda(0, miLista));
+    }
+
+    @Test
+    void cambioLambdaTest3() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(3);
+        miLista.add(2);
+        miLista.add(1);
+        
+        assertEquals(1, CambioMoneda.cambioLambda(1, miLista));
+    }
+
+    @Test
+    void cambioLambdaTest4() {
+
+        ArrayList<Integer> miLista = new ArrayList<Integer>();
+        miLista.add(6);
+        miLista.add(4);
+        miLista.add(2);
+        miLista.add(1);
+
+        assertEquals(3, CambioMoneda.cambioLambda(13, miLista));
+    }
+}
